a4f1ab753d897fbe10c6f71f32116be12500b267
kwanjung <kwanjungkim@naver.com>
2025-09-03 11:41:17 +0900

    Initial commit

---
+++ src/main/java/com/fitian/burntz/BurntzApplication.java
@@ -0,0 +1,13 @@
+package com.fitian.burntz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurntzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurntzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitian/burntz/BurntzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitian.burntz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurntzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
