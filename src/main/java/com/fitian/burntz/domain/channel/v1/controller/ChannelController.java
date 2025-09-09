7a1bed00e087a01f3a71a492e883900a9fa5924b
kwanjung <kwanjungkim@naver.com>
2025-09-09 18:13:20 +0900

    feat : 박스내 유저가 참여중인 채널 목록 반환, channelPk로 해당 채널의 참여자 목록 반환 기능 추가

--- src/main/java/com/fitian/burntz/domain/channel/v1/controller/ChannelController.java
+++ src/main/java/com/fitian/burntz/domain/channel/v1/controller/ChannelController.java
@@ -1,17 +1,19 @@
 package com.fitian.burntz.domain.channel.v1.controller;
 
+import com.fitian.burntz.domain.channel.entity.ChannelParticipant;
 import com.fitian.burntz.domain.channel.v1.dto.ChannelCreateRequest;
 import com.fitian.burntz.domain.channel.service.ChannelService;
+import com.fitian.burntz.domain.channel.v1.dto.ChannelListResponse;
+import com.fitian.burntz.domain.member.service.MemberService;
 import com.fitian.burntz.global.common.response.ApiResponse;
 import com.fitian.burntz.global.security.core.CustomUserDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author : 김관중
@@ -27,13 +29,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChannelController {
 
     private final ChannelService channelService;
+    private final MemberService memberService;
 
     @PostMapping()
-    public ResponseEntity<ApiResponse<Void>> createChannel(
+    public ApiResponse<Void> createChannel(
             @Valid @RequestBody ChannelCreateRequest request,
-            @AuthenticationPrincipal CustomUserDetails userDetails
-            ) {
+            @AuthenticationPrincipal CustomUserDetails userDetails ) {
         channelService.createChannel(request,  userDetails);
-        return ResponseEntity.ok(ApiResponse.success(null, "채널 개설 완료."));
+        return ApiResponse.success(null, "채널 개설 완료.");
+    }
+
+    @GetMapping()
+    public ResponseEntity<ApiResponse<List<ChannelListResponse>>> getChannels(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestParam Long boxPk) {
+        return ResponseEntity.ok(ApiResponse.success(channelService.getChannels(userDetails, boxPk),"채널 목록 반환 완료."));
+    }
+
+
+    @GetMapping("/{channelPk}/participants")
+    public ResponseEntity<ApiResponse<List<ChannelParticipant>>> getParticipants(
+            @PathVariable Long channelPk,
+            @AuthenticationPrincipal CustomUserDetails userDetails ) {
+        return ResponseEntity.ok(ApiResponse.success(channelService.getParticipants(userDetails, channelPk),"채널 참여자 목록 반환 완료."));
     }
 }
