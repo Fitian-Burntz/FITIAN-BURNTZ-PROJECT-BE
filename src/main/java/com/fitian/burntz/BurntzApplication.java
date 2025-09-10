d5d8b5a72b8c2ccf129ad8e1485a880cf0622954
Nam-I <jamong9669@naver.com>
2025-09-10 15:15:36 +0900

    feature: 애플로그인 개발. 테스트 필요

--- src/main/java/com/fitian/burntz/BurntzApplication.java
+++ src/main/java/com/fitian/burntz/BurntzApplication.java
@@ -2,9 +2,14 @@ package com.fitian.burntz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.client.HttpClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.client.RestClientAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        HttpClientAutoConfiguration.class,
+        RestClientAutoConfiguration.class
+})
 @EnableJpaAuditing
 public class BurntzApplication {
 
