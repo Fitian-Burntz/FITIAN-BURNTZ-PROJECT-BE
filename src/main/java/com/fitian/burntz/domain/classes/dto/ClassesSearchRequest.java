5dab28d2b13079bb34e340396d83a20207bd068c
kwanjung <kwanjungkim@naver.com>
2025-09-15 17:44:27 +0900

    feat : classes 관련 기본 mvc 작성

---
+++ src/main/java/com/fitian/burntz/domain/classes/dto/ClassesSearchRequest.java
@@ -0,0 +1,37 @@
+package com.fitian.burntz.domain.classes.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+/**
+ * @author : 김관중
+ * @packageName : com.fitian.burntz.domain.classes.dto
+ * @fileName : ClassesSearchRequest
+ * @date : 2025-09-15
+ * @description : 수업 호출 DTO 입니다
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClassesSearchRequest {
+
+    @NotBlank(message = "boxPK must not be blank")
+    private Long boxPK;
+
+    @NotNull(message = "startDate must not be blank")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @NotNull(message = "endDate must not be blank")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+}
---
+++ src/main/java/com/fitian/burntz/domain/classes/repository/ClassesRepository.java
@@ -0,0 +1,25 @@
+package com.fitian.burntz.domain.classes.repository;
+
+import com.fitian.burntz.domain.box.entity.Box;
+import com.fitian.burntz.domain.classes.entity.Classes;
+import com.fitian.burntz.global.common.entity.BaseTime.Yn;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * @author : 김관중
+ * @packageName : com.fitian.burntz.domain.classes.repository
+ * @fileName : ClassesRepository
+ * @date : 2025-09-15
+ * @description : 수업 리포지토리입니다.
+ */
+public interface ClassesRepository extends JpaRepository<Classes, Long> {
+
+    List<Classes> findByBoxAndClassDateBetweenAndDeletedYN(
+            Box box, LocalDate startDate, LocalDate endDate, Yn deletedYN);
+
+    List<Classes> findByBoxBoxPkAndClassDateBetweenAndDeletedYN(
+            Long boxPk, LocalDate startDate, LocalDate endDate, Yn deletedYN);
+}
