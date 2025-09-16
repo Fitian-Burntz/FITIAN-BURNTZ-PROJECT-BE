28c7c0e212bd4d7832b48a23f24c4ca67f12bad1
june <0721hjp@naver.com>
2025-09-16 10:22:38 +0900

    Feat: 구매기능 구현중

---
+++ src/main/java/com/fitian/burntz/domain/box/repository/BoxSubscriptionRepository.java
@@ -0,0 +1,13 @@
+package com.fitian.burntz.domain.box.repository;
+
+import com.fitian.burntz.domain.box.entity.BoxSubscription;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface BoxSubscriptionRepository extends JpaRepository<BoxSubscription, Long> {
+
+  @Query("select bs from BoxSubscription bs join fetch bs.member where bs.member.memberPk = :ownerMemberId")
+  Optional<BoxSubscription> findByOwnerMemberId(Long ownerMemberId);
+
+}
