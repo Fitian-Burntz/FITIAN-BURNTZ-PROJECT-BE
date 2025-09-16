38c1d02866d1d3d885df8050bf13d2a5258fceb7
june <0721hjp@naver.com>
2025-09-16 10:22:38 +0900

    Feat: 구매기능 구현중

--- src/main/java/com/fitian/burntz/infra/payment/service/PaymentService.java
+++ src/main/java/com/fitian/burntz/infra/payment/service/PaymentService.java
@@ -1,5 +1,20 @@
 package com.fitian.burntz.infra.payment.service;
 
+import com.fitian.burntz.domain.box.repository.BoxSubscriptionRepository;
+import com.fitian.burntz.domain.member.repository.MemberRepository;
+import com.fitian.burntz.infra.payment.v1.dto.WebhookPurchaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class PaymentService {
+
+  private final BoxSubscriptionRepository boxSubscriptionRepository;
+  private final MemberRepository memberRepository;
+
+  public void handlePuchaseWebhook(WebhookPurchaseResponse webhookPurchaseResponse) {
+
+  }
 
 }
