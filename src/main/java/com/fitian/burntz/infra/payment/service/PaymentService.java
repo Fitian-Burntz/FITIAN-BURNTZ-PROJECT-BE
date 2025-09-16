5cd4696a27ba052d65adf43b207401d837641abd
june <0721hjp@naver.com>
2025-09-16 17:54:16 +0900

    feat: 결제정보 응답 필드 추가 (boxPk)

--- src/main/java/com/fitian/burntz/infra/payment/service/PaymentService.java
+++ src/main/java/com/fitian/burntz/infra/payment/service/PaymentService.java
@@ -15,6 +15,13 @@ public class PaymentService {
 
   public void handlePuchaseWebhook(WebhookPurchaseResponse webhookPurchaseResponse) {
 
+    System.out.println("상품코드 :  = " + webhookPurchaseResponse.getEvent().getProductId());
+    System.out.println("상품가격 :  = " + webhookPurchaseResponse.getEvent().getPrice() + "달러");
+    System.out.println("구매처 = " + webhookPurchaseResponse.getEvent().getStore());
+    System.out.println("구매자 아이디 = " + webhookPurchaseResponse.getEvent().getOwnerMemberId());
+    System.out.println("이벤트 타입 = " + webhookPurchaseResponse.getEvent().getType().getValue());
+    System.out.println("구매한 박스 pk = " + webhookPurchaseResponse.getEvent().getSubscriberAttributes().getBoxPk().getValue());
+
   }
 
 }
