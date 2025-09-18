03d36132b95da2015914936511df6e6d83c23550
june <0721hjp@naver.com>
2025-09-18 10:03:38 +0900

    chore: 일단 구매 취소 기능은 보류

--- src/main/java/com/fitian/burntz/infra/payment/v1/controller/PaymentController.java
+++ src/main/java/com/fitian/burntz/infra/payment/v1/controller/PaymentController.java
@@ -34,15 +34,15 @@ public class PaymentController {
     return ResponseEntity.ok().build();
   }
 
-  @PostMapping("/webhook/cancel")
-  public ResponseEntity<?> handleCancelWebhook(@RequestBody WebhookPurchaseResponse webhookPurchaseResponse) {
-    System.out.println("상품코드 :  = " + webhookPurchaseResponse.getEvent().getProductId());
-    System.out.println("상품가격 :  = " + webhookPurchaseResponse.getEvent().getPrice() + "달러");
-    System.out.println("구매처 = " + webhookPurchaseResponse.getEvent().getStore());
-    System.out.println("구매자 아이디 = " + webhookPurchaseResponse.getEvent().getOwnerMemberId());
-    System.out.println("이벤트 타입 = " + webhookPurchaseResponse.getEvent().getType().getValue());
-    return ResponseEntity.ok().build();
-  }
+//  @PostMapping("/webhook/cancel")
+//  public ResponseEntity<?> handleCancelWebhook(@RequestBody WebhookPurchaseResponse webhookPurchaseResponse) {
+//    System.out.println("상품코드 :  = " + webhookPurchaseResponse.getEvent().getProductId());
+//    System.out.println("상품가격 :  = " + webhookPurchaseResponse.getEvent().getPrice() + "달러");
+//    System.out.println("구매처 = " + webhookPurchaseResponse.getEvent().getStore());
+//    System.out.println("구매자 아이디 = " + webhookPurchaseResponse.getEvent().getOwnerMemberId());
+//    System.out.println("이벤트 타입 = " + webhookPurchaseResponse.getEvent().getType().getValue());
+//    return ResponseEntity.ok().build();
+//  }
 
 
 
