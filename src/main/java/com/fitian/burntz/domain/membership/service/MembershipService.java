7c25e16ef047a5fab218175bd08806743204d90e
kwanjung <kwanjungkim@naver.com>
2025-09-19 16:15:52 +0900

    feat : membership history 반환 및 각 기능에 로그 저장

--- src/main/java/com/fitian/burntz/domain/membership/service/MembershipService.java
+++ src/main/java/com/fitian/burntz/domain/membership/service/MembershipService.java
@@ -1,5 +1,7 @@
 package com.fitian.burntz.domain.membership.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fitian.burntz.domain.box.entity.Box;
 import com.fitian.burntz.domain.box.enums.MemberRole;
 import com.fitian.burntz.domain.box.repository.BoxRepository;
@@ -8,6 +10,7 @@ import com.fitian.burntz.domain.member.entity.MemberList;
 import com.fitian.burntz.domain.member.repository.MemberListRepository;
 import com.fitian.burntz.domain.member.repository.MemberRepository;
 import com.fitian.burntz.domain.membership.entity.MembershipHistory;
+import com.fitian.burntz.domain.membership.enums.HistoryActionType;
 import com.fitian.burntz.domain.membership.repository.MembershipHistoryRepository;
 import com.fitian.burntz.domain.membership.v1.dto.*;
 import com.fitian.burntz.domain.membership.entity.Membership;
@@ -17,6 +20,7 @@ import com.fitian.burntz.global.exception.ErrorCode;
 import com.fitian.burntz.global.exception.ValidationException;
 import com.fitian.burntz.global.security.core.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,11 +35,13 @@ import java.util.List;
  * @description : 멤버십 서비스입니다.
  */
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class MembershipService {
 
+    private final ObjectMapper objectMapper;
     private final MemberRepository memberRepository;
     private final BoxRepository boxRepository;
     private final MembershipRepository membershipRepository;
@@ -87,6 +93,27 @@ public class MembershipService {
                 .build();
 
         membershipRepository.save(membership);
+
+        try {
+            Member createdBy = memberRepository.findById(userDetails.getMemberPk())
+                    .orElseThrow(() -> new ValidationException(ErrorCode.USER_NOT_FOUND));
+
+            String newValue = objectMapper.writeValueAsString(membership);
+            MembershipHistory history = MembershipHistory.builder()
+                    .membership(membership)
+                    .actionType(HistoryActionType.CREATE)
+                    .preValue(null)
+                    .newValue(newValue)
+                    .memo(request.getMemo())
+                    .period(request.getPeriod())
+                    .createdBy(createdBy)
+                    .build();
+            historyRepository.save(history);
+        } catch (JsonProcessingException e) {
+            log.error("Failed to save Membership History. membershipPk={}, boxPk={}, memberPk={}",
+                    membership.getMembershipPk(), boxPk, memberPk);
+            throw new RuntimeException("Failed to save Membership History.", e);
+        }
     }
 
     public void updateMembership(Long boxPk, Long memberPk, MembershipUpdateRequest request, CustomUserDetails userDetails){
@@ -100,7 +127,29 @@ public class MembershipService {
 
         if(!memberPk.equals(membership.getMember().getMemberPk())) throw new ValidationException(ErrorCode.USER_NOT_FOUND);
 
-        membership.updateFrom(request);
+        try {
+            String preValue = objectMapper.writeValueAsString(membership);
+            membership.updateFrom(request);
+            String newValue = objectMapper.writeValueAsString(membership);
+
+            Member createdBy = memberRepository.findById(userDetails.getMemberPk())
+                    .orElseThrow(() -> new ValidationException(ErrorCode.USER_NOT_FOUND));
+
+            MembershipHistory history = MembershipHistory.builder()
+                    .membership(membership)
+                    .actionType(HistoryActionType.UPDATE)
+                    .preValue(preValue)
+                    .newValue(newValue)
+                    .memo(request.getMemo())
+                    .period(request.getPeriod())
+                    .createdBy(createdBy)
+                    .build();
+            historyRepository.save(history);
+        } catch (JsonProcessingException e) {
+            log.error("Failed to save Membership History. membershipPk={}, boxPk={}, memberPk={}",
+                    membership.getMembershipPk(), boxPk, memberPk);
+            throw new RuntimeException("Failed to save Membership History.", e);
+        }
     }
 
     public void deleteMembership(Long boxPk, Long memberPk, MembershipIdentifierRequest request, CustomUserDetails userDetails){
@@ -114,14 +163,56 @@ public class MembershipService {
 
         if(!memberPk.equals(membership.getMember().getMemberPk())) throw new ValidationException(ErrorCode.USER_NOT_FOUND);
 
-        membership.markDeleted();
+        try{
+            String preValue = objectMapper.writeValueAsString(membership);
+            membership.markDeleted();
+
+            Member createdBy = memberRepository.findById(userDetails.getMemberPk())
+                    .orElseThrow(() -> new ValidationException(ErrorCode.USER_NOT_FOUND));
+
+            MembershipHistory history = MembershipHistory.builder()
+                    .membership(membership)
+                    .actionType(HistoryActionType.DELETE)
+                    .preValue(preValue)
+                    .newValue(null)
+                    .memo(null)
+                    .period(0)
+                    .createdBy(createdBy)
+                    .build();
+            historyRepository.save(history);
+        } catch (JsonProcessingException e) {
+            log.error("Failed to save Membership History. membershipPk={}, boxPk={}, memberPk={}",
+                    membership.getMembershipPk(), boxPk, memberPk);
+            throw new RuntimeException("Failed to save Membership History.", e);
+        }
     }
 
     public List<MembershipHistoryResponse> getMembershipHistory(Long boxPk, Long memberPk, MembershipIdentifierRequest request, CustomUserDetails userDetails) {
 
-       List<MembershipHistory> historyList = historyRepository.findAllByMembershipMembershipPk(request.getMembershipPk());
+        //존재하는 회원인지 검증
+        boolean exist = memberListRepository.existsByBoxBoxPkAndMemberMemberPkAndDeletedYN(boxPk, userDetails.getMemberPk(), BaseTime.Yn.N);
+        if(!exist) throw new ValidationException(ErrorCode.ACCESS_DENIED);
 
-       List<MembershipHistoryResponse> responseList = new ArrayList<>();
-       return responseList;
+        boolean exist2 = memberListRepository.existsByBoxBoxPkAndMemberMemberPkAndDeletedYN(boxPk, memberPk, BaseTime.Yn.N);
+        if(!exist2) throw new ValidationException(ErrorCode.ACCESS_DENIED);
+
+        List<MembershipHistory> historyList = historyRepository.findAllByMembershipMembershipPk(request.getMembershipPk());
+
+        List<MembershipHistoryResponse> responseList = new ArrayList<>();
+
+        for(MembershipHistory mh : historyList) {
+            MembershipHistoryResponse response = MembershipHistoryResponse.builder()
+                   .membershipPk(mh.getMembershipHistoryPk())
+                   .actionType(mh.getActionType())
+                   .preValue(mh.getPreValue())
+                   .newValue(mh.getNewValue())
+                   .memo(mh.getMemo())
+                   .period(mh.getPeriod())
+                   .createdBy(mh.getCreatedBy())
+                   .createdAt(mh.getCreatedAt())
+                   .build();
+           responseList.add(response);
+        }
+        return responseList;
     }
 }
