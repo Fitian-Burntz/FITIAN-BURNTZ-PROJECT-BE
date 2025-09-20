83cfdac6cc7202b8b3991c99943e0e1e6dc0cbfd
dev-ssj <ju981010@naver.com>
2025-09-20 15:15:17 +0900

    feat : Record CRUD 구현

--- src/main/java/com/fitian/burntz/domain/record/repository/RecordRepository.java
+++ src/main/java/com/fitian/burntz/domain/record/repository/RecordRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,4 +36,5 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
             "join fetch w.box b " +
             "where r.recordPk = :recordPk")
     Optional<Record> findByIdWithWodAndBox(@Param("recordPk") Long recordPk);
+
 }
