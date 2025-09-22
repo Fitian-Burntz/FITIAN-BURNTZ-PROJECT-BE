67d07139d214388420f5cd58f7d682b46e8b864c
kwanjung <kwanjungkim@naver.com>
2025-09-22 17:01:24 +0900

    fix : dto 수정 및 membership 응답시 boxNickname 반환

--- src/main/java/com/fitian/burntz/domain/membership/v1/dto/MembershipHistoryResponse.java
+++ src/main/java/com/fitian/burntz/domain/membership/v1/dto/MembershipHistoryResponse.java
@@ -24,12 +24,28 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Schema(name = "MembershipHistoryResponse", description = "멤버십 로그 응답")
 public class MembershipHistoryResponse {
+
+    @Schema(description = "멤버십 PK", example = "1")
     private Long membershipPk;
+
+    @Schema(description = "멤버십 로그 사유", example = "UPDATE", allowableValues = {"UPDATE","DELETE","CREATE"})
     private HistoryActionType actionType;
+
+    @Schema(description = "이전 기록", example = "Json 형태")
     private String preValue;
+
+    @Schema(description = "변경된 기록", example = "Json 형태")
     private String newValue;
+
+    @Schema(description = "메모", example = "변경 사유")
     private String memo;
+
+    @Schema(description = "기간", example = "1")
     private Integer period;
+
+    @Schema(description = "변경한 사람", example = "Json member 객체")
     private Member createdBy;
+
+    @Schema(description = "생성일시(서버 기준)", example = "2025-09-16T15:00:00")
     private LocalDateTime createdAt;
 }
