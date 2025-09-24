3b1a64fd58b0b2121e605c94e053a8cc22ad9bac
kwanjung <kwanjungkim@naver.com>
2025-09-24 17:13:18 +0900

    fix : SwaggerConfig url 수정, AuthDocs ApiResponse 부분 제거

--- src/main/java/com/fitian/burntz/domain/auth/docs/AuthDocs.java
+++ src/main/java/com/fitian/burntz/domain/auth/docs/AuthDocs.java
@@ -2,9 +2,6 @@ package com.fitian.burntz.domain.auth.docs;
 
 import com.fitian.burntz.domain.auth.dto.LoginResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,22 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "Auth 관련 api 입니다.", description = "login,logout,refresh 인증과 관련된 일을 수행합니다.")
 public interface AuthDocs {
-
-    @ApiResponse(
-            responseCode = "200",
-            description = "호출에 성공한 경우",
-            content = @Content(
-                    mediaType = "application/json",
-                    examples = @ExampleObject(
-                            value =
-                                    """
-                                        {
-                                            "message": "Test API"
-                                        }
-                                    """
-                    )
-            )
-    )
 
     @Operation(summary = "소셜로그인 수행 api 입니다.", description = "google/apple 소셜로그인을 수행 합니다.")
     public ResponseEntity<com.fitian.burntz.global.common.response.ApiResponse<LoginResponse>> loginWithSocial(
