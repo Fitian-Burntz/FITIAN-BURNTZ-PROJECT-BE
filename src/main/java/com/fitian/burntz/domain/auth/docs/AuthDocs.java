6b612f2187bbb88a481ef01c1d755c0a28b99887
kwanjung <kwanjungkim@naver.com>
2025-09-24 17:21:48 +0900

    fix : AuthController 반환자 변경 및 docs 수정

--- src/main/java/com/fitian/burntz/domain/auth/docs/AuthDocs.java
+++ src/main/java/com/fitian/burntz/domain/auth/docs/AuthDocs.java
@@ -1,6 +1,9 @@
 package com.fitian.burntz.domain.auth.docs;
 
+import com.fitian.burntz.domain.auth.dto.AuthTokenResponse;
 import com.fitian.burntz.domain.auth.dto.LoginResponse;
+import com.fitian.burntz.domain.auth.dto.LogoutResponse;
+import com.fitian.burntz.global.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -11,24 +14,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface AuthDocs {
 
     @Operation(summary = "소셜로그인 수행 api 입니다.", description = "google/apple 소셜로그인을 수행 합니다.")
-    ResponseEntity<?> loginWithSocial(
+    public ResponseEntity<ApiResponse<LoginResponse>> loginWithSocial(
             @RequestHeader(value = "Authorization", required = false) String authorization,
             @RequestParam("provider") String provider,
             @RequestParam(value = "deviceId", required = false) String deviceId);
 
 
     @Operation(summary = "현재 기기에서만 로그아웃 api 입니다.", description = "현재 기기에서만 로그아웃 합니다.")
-    ResponseEntity<?> logoutCurrentDevice(
+    public ResponseEntity<ApiResponse<LogoutResponse>> logoutCurrentDevice(
             @RequestHeader(value = "Authorization", required = false) String authorization,
             @RequestParam(value = "deviceId", required = false) String deviceId);
 
 
     @Operation(summary = "모든 기기에서 로그아웃 api 입니다.", description = "해당 멤버의 모든 기기에서 로그아웃 합니다.")
-    ResponseEntity<?> logoutAllDevices(@RequestHeader(value = "Authorization", required = false) String authorization);
+    public ApiResponse<Void> logoutAllDevices(@RequestHeader(value = "Authorization", required = false) String authorization);
 
 
     @Operation(summary = "refreshToken 갱신 api 입니다.", description = "현재 로그인된 계정의 refreshToken을 갱신합니다.")
-    ResponseEntity<?> refreshTokenBased(
+    public ResponseEntity<ApiResponse<AuthTokenResponse>> refreshTokenBased(
             @RequestHeader(value = "Authorization", required = false) String authorization,
             @RequestParam(value = "deviceId", required = false) String deviceId);
 
