d164d591e21d0b0da9edfc03af1b8afe8084c918
june <0721hjp@naver.com>
2025-09-25 16:48:35 +0900

    docs: 구매로그 조회 api 스웨거 문서화

--- src/main/java/com/fitian/burntz/infra/payment/docs/PaymentDocs.java
+++ src/main/java/com/fitian/burntz/infra/payment/docs/PaymentDocs.java
@@ -3,13 +3,19 @@ package com.fitian.burntz.infra.payment.docs;
 import com.fitian.burntz.global.common.response.ApiResponse;
 import com.fitian.burntz.infra.payment.dto.response.PurchaseLogResponse;
 import com.fitian.burntz.infra.payment.v1.dto.WebhookPurchaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 
+@Tag(name = "구매 관련 api 입니다.", description = "구매 이력을 조회하거나 웹훅을 통해 결제 완료 데이터를 받습니다.")
 public interface PaymentDocs {
 
+  @Operation(summary = "결제 완료 웹훅 엔드포인트", description = "해당 웹훅을 통해 결제 완료 데이터를 받습니다.")
   public ResponseEntity<?> handlePuchaseWebhook(WebhookPurchaseResponse webhookPurchaseResponse, HttpServletRequest request);
+
+  @Operation(summary = "구매 이력 조회", description = "boxPk로 해당 박스의 구매 이력을 조회합니다.")
   public ApiResponse<List<PurchaseLogResponse>> getPurchaseLog(Long memberPk);
 }
+
