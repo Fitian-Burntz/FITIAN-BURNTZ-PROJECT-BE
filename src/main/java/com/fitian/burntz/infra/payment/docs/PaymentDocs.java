fba6a9deb67b7b2a87448ae1a7e45a38ef0503e5
june <0721hjp@naver.com>
2025-09-25 14:50:42 +0900

    chore: 결제완료 웹훅 로그 추가

--- src/main/java/com/fitian/burntz/infra/payment/docs/PaymentDocs.java
+++ src/main/java/com/fitian/burntz/infra/payment/docs/PaymentDocs.java
@@ -1,10 +1,11 @@
 package com.fitian.burntz.infra.payment.docs;
 
 import com.fitian.burntz.infra.payment.v1.dto.WebhookPurchaseResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 
 public interface PaymentDocs {
 
-  public ResponseEntity<?> handlePuchaseWebhook(WebhookPurchaseResponse webhookPurchaseResponse);
+  public ResponseEntity<?> handlePuchaseWebhook(WebhookPurchaseResponse webhookPurchaseResponse, HttpServletRequest request);
 
 }
