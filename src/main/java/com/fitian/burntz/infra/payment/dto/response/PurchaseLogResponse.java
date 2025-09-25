2c1e543db6040cd28941af06d3c60e2a576d0460
june <0721hjp@naver.com>
2025-09-25 16:25:07 +0900

    feat: 결제 내역 조회 응답 dto 생성

---
+++ src/main/java/com/fitian/burntz/infra/payment/dto/response/PurchaseLogResponse.java
@@ -0,0 +1,16 @@
+package com.fitian.burntz.infra.payment.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class PurchaseLogResponse {
+
+  private LocalDateTime purchasedAt; // = 구매일자 = createdAt(로그 db 필드명)
+  private String productId; // revenuecat 상품 아이디
+  private String price; // 결제 금액
+}
