2c3c20dc16a714eecc6c63a0286fe58ddc47a1c2
june <0721hjp@naver.com>
2025-09-25 14:22:03 +0900

    feat: 결제완료 웹훅 토큰 검증 로직 추가

--- src/main/java/com/fitian/burntz/infra/payment/service/PaymentService.java
+++ src/main/java/com/fitian/burntz/infra/payment/service/PaymentService.java
@@ -11,8 +11,10 @@ import com.fitian.burntz.domain.member.entity.Member;
 import com.fitian.burntz.domain.member.repository.MemberRepository;
 import com.fitian.burntz.global.exception.ErrorCode;
 import com.fitian.burntz.global.exception.ValidationException;
+import com.fitian.burntz.global.security.jwt.JwtTokenProvider;
 import com.fitian.burntz.infra.payment.enums.PaymentStore;
 import com.fitian.burntz.infra.payment.v1.dto.WebhookPurchaseResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,34 +31,45 @@ public class PaymentService {
   private final SubscriptionEventLogRepository subscriptionEventLogRepository;
   private final MemberRepository memberRepository;
 
+  private final JwtTokenProvider jwtTokenProvider;
+
   /**
    * [결제 완료 웹훅 처리 메서드]
    * @param webhookPurchaseResponse 결제 완료 웹훅 데이터
    * 결제완료 처리 흐름..
-   * 1. 멤버가 존재하는지 확인
-   * 2. 박스가 존재하는지 확인
-   * 3. box_subscription 테이블 업데이트 또는 삽입
-   * 4. 구독 정보 저장
-   * 5. 구독 로그 저장
-   * 6. 최종 BOX 구독상태 변경
+   * 1. 토큰 검증
+   * 2. 멤버가 존재하는지 확인
+   * 3. 박스가 존재하는지 확인
+   * 4. box_subscription 테이블 업데이트 또는 삽입
+   * 5. 구독 정보 저장
+   * 6. 구독 로그 저장
+   * 7. 최종 BOX 구독상태 변경
    */
   @Transactional
-  public void handlePuchaseWebhook(WebhookPurchaseResponse webhookPurchaseResponse) {
+  public void handlePuchaseWebhook(WebhookPurchaseResponse webhookPurchaseResponse, HttpServletRequest request) {
     log.info("\n" + "결제완료 데이터 수신" + "\n" + "주문자 ID : " + webhookPurchaseResponse.getEvent().getOwnerMemberId() + "\n" + "박스 pk : " + webhookPurchaseResponse.getEvent().getSubscriberAttributes().getBoxPk().getValue());
 
-    // 1. 멤버가 존재하는지 확인
+    // 1. 토큰 검증
+    String token = extractToken(request);
+    if(!jwtTokenProvider.validateToken(token)) {
+      log.error("토큰 검증 실패 - 결제완료 웹훅 처리중 토큰이 유효하지 않습니다.(" + "구매한 box pk : " + webhookPurchaseResponse.getEvent().getSubscriberAttributes().getBoxPk().getValue() + ")" + "(" + "구매자 pk : " + webhookPurchaseResponse.getEvent().getOwnerMemberId() + ")");
+      throw new ValidationException(ErrorCode.TOKEN_INVALID);
+    }
+
+
+    // 2. 멤버가 존재하는지 확인
     String ownerMemberId = webhookPurchaseResponse.getEvent().getOwnerMemberId();
     Long ownerMemberIdToLong = Long.parseLong(ownerMemberId);
     Member member = memberRepository.findById(ownerMemberIdToLong)
         .orElseThrow(() -> new ValidationException(ErrorCode.USER_NOT_FOUND));
 
-    // 2. 박스가 존재하는지 확인
+    // 3. 박스가 존재하는지 확인
     String boxPk = webhookPurchaseResponse.getEvent().getSubscriberAttributes().getBoxPk().getValue();
     Long boxPkToLong = Long.parseLong(boxPk);
     Box box = boxRepository.findById(boxPkToLong)
         .orElseThrow(() -> new ValidationException(ErrorCode.BOX_NOT_FOUND));
 
-    // 3. box_subscription 테이블 업데이트 또는 삽입
+    // 4. box_subscription 테이블 업데이트 또는 삽입
     String productId = webhookPurchaseResponse.getEvent().getProductId();
     PaymentStore store = webhookPurchaseResponse.getEvent().getStore();
     SubscriptionStatus subscriptionStatus = SubscriptionStatus.ACTIVE;
@@ -69,7 +82,7 @@ public class PaymentService {
 
     SubscriptionEventLog subscriptionEventLog = SubscriptionEventLog.from(webhookPurchaseResponse, member, box);
 
-    // 4. 박스 구독 정보 저장
+    // 5. 박스 구독 정보 저장
     if(boxSubscriptionRepository.findByBoxPk(boxPkToLong).isPresent()) {
       log.info("박스 구독 정보 저장 중 - 기존 구독 정보가 존재하여 업데이트를 진행합니다.(" + "구매한 box pk : " + boxPk + ")" + "(" + "구매자 pk : " + ownerMemberId + ")");
       BoxSubscription oldBoxSubscription = boxSubscriptionRepository.findByBoxPk(boxPkToLong)
@@ -81,13 +94,33 @@ public class PaymentService {
       boxSubscriptionRepository.save(boxSubscription);
     }
 
-    // 5. 박스 구독 로그 저장
+    // 6. 박스 구독 로그 저장
     log.info("박스 구독 로그 저장 중 - 구매 로그를 저장합니다.(" + "구매한 box pk : " + boxPk + ")" + "(" + "구매자 pk : " + ownerMemberId + ")");
     subscriptionEventLogRepository.save(subscriptionEventLog);
 
-    // 6. 최종 BOX 구독상태 변경
+    // 7. 최종 BOX 구독상태 변경
     box.subscribe();
 
+  }
+
+  /**
+   * HTTP 요청 헤더에서 JWT 토큰을 추출하는 메서드입니다.
+   *
+   * @param request HTTP 요청
+   * @return 추출된 JWT 토큰
+   */
+  public String extractToken(HttpServletRequest request) {
+    String authHeader = request.getHeader("Authorization");
+
+    // Authorization 헤더가 없거나 "Bearer "로 시작하지 않는 경우 예외 처리
+    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+      log.error("Authorization 헤더가 없거나 Bearer 로 시작하지 않습니다.");
+      throw new ValidationException(ErrorCode.TOKEN_EXTRACTION_FAILED);
+    }
+
+    String token = authHeader.substring(7);
+
+    return token;
   }
 
 }
