95fe9ba528eb6bd7030659293f0d7fe5d455021a
june <0721hjp@naver.com>
2025-09-30 10:32:00 +0900

    chore: admin 페이지를 위한 cors 설정 추가

---
+++ src/main/java/com/fitian/burntz/global/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.fitian.burntz.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CORS 설정을 위한 클래스입니다.
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:8080",
+                "http://localhost:3000",
+                "http://localhost:5173",
+                "https://server.burntz.app"
+            )
+            .allowCredentials(true)
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .maxAge(3600);
+    }
+}
